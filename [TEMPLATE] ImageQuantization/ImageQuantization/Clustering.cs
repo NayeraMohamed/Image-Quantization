3d1774c5fa26fc77e4839e2c530363dc99884d84
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2019-12-22 02:11:57 +0200

    Update Clustering.cs

--- [TEMPLATE] ImageQuantization/ImageQuantization/Clustering.cs
+++ [TEMPLATE] ImageQuantization/ImageQuantization/Clustering.cs
@@ -11,11 +11,12 @@ namespace ImageQuantization
         static double MST_Sum = MST.FindMinimumSpanningTree();
         static Dictionary<string, KeyValuePair<string, double>> MSTree = MST.GetMST();
         static Dictionary<string, RGBPixel> MSTHelper = MST.GetMSTHelper();
-        
-         // To be seen by DFS fn && pelletteGeneration fn
+        public static Dictionary<string, RGBPixel> represntativeColor = new Dictionary<string, RGBPixel>();//init later
+
+        // To be seen by DFS fn && pelletteGeneration fn
         static Stack<string> clusterColors;
         static Dictionary<string, bool> discovered = new Dictionary<string, bool>(noDistinctColors);
-        
+
         static int DescendingOrder(KeyValuePair<double, string> node1, KeyValuePair<double, string> node2)
         {
             return node2.Key.CompareTo(node1.Key); //for descending order
@@ -35,46 +36,49 @@ namespace ImageQuantization
             edges.Sort(DescendingOrder);
             return edges;
         }
-        public static void ProduceKClusters(int k)
+        public static int ProduceKClusters(int k)
         {
             List<KeyValuePair<double, string>> edges = ExtractMSTEdges();
-            for(int i = 0; i < k - 1; ++i)
+            for (int i = 0; i < k; ++i)
             {
                 string source = edges[i].Value;
+                double x = edges[i].Key;
                 MSTree.Remove(source);
             }
+            return MSTree.Count();
         }
         public static List<RGBPixel> paletteGeneration()
-        {
-            List<RGBPixel> pallette  = new List<RGBPixel>(); // initialize the size by K clusters 
-             
+        { 
+            int noClusters = ProduceKClusters(1);
+            List<RGBPixel> pallette = new List<RGBPixel>(noClusters); // initialize the size by K clusters 
 
             // initialization of the discovered list by false
-            List<KeyValuePair<string, KeyValuePair<string, double>>> clusters = MSTree.ToList();
-            foreach (KeyValuePair<string, KeyValuePair<string, double>> node in clusters)
+            List<KeyValuePair<string, RGBPixel>> clusters = MSTHelper.ToList();
+            foreach (KeyValuePair<string, RGBPixel> node in clusters)
             {
                 string key = node.Key;
                 bool visited = false;
                 discovered.Add(key, visited);
+                represntativeColor.Add(key, node.Value);
             }
 
 
             // traversing each cluster
-            List<KeyValuePair<string, bool>> vertices = discovered.ToList();
-            foreach (KeyValuePair<string, bool> vertex in vertices)
+            List<KeyValuePair<string, KeyValuePair<string, double>>> vertices = MSTree.ToList();
+            foreach (KeyValuePair<string, KeyValuePair<string, double>> vertex in vertices)
             {
-                if (vertex.Value == false)
+                if (discovered[vertex.Key] == false)
                 {
-                    clusterColors = new Stack<string>();
-                    
+                    clusterColors = new Stack<string>();//init
+
                     int clusterRed = 0;
                     int clusterGreen = 0;
                     int clusterBlue = 0;
 
-                    DFS(vertex.Key, clusterRed, clusterGreen, clusterBlue);
+                    DFS(vertex.Key, ref clusterRed, ref clusterGreen, ref clusterBlue);
 
                     //  the average ??
-                    RGBPixel newColor;
+                    RGBPixel newColor = new RGBPixel();
                     int clusterNodesCount = clusterColors.Count;  // indicates # Distinct colors in this cluster 
                     newColor.red = (byte)(clusterRed / clusterNodesCount);
                     newColor.blue = (byte)(clusterBlue / clusterNodesCount);
@@ -83,23 +87,29 @@ namespace ImageQuantization
 
 
                     // to extract the stack values before the next loop 
-                    //
-                    //
+                    while (clusterColors.Count != 0)//anaa
+                    {
+                        string basicColor = clusterColors.Pop();
+                        
+                        represntativeColor[basicColor] = newColor;
+                        
+                    }
                 }
 
                 else
+                {
                     continue;
-
-
+                }
             }
+
             return pallette;
         }
 
-        public static void DFS(string S, int clusterRed, int clusterGreen, int clusterBlue)
+        static void DFS(string S, ref int clusterRed, ref int clusterGreen, ref int clusterBlue)
         {
             discovered[S] = true;
             clusterColors.Push(S);
-         
+
 
             byte red = MSTHelper[S].red;
             byte green = MSTHelper[S].green;
@@ -109,13 +119,15 @@ namespace ImageQuantization
             clusterGreen += green;
             clusterBlue += blue;
 
-            
-            string adjacentNode = MSTree[S].Key;
-            if (discovered.ContainsKey(adjacentNode) && discovered[adjacentNode] == false)
+            if(MSTree.ContainsKey(S))
             {
-                DFS(adjacentNode, clusterRed, clusterGreen, clusterBlue);
+                string adjacentNode = MSTree[S].Key;
+                if (discovered[adjacentNode] == false)
+                {
+                    DFS(adjacentNode, ref clusterRed, ref clusterGreen, ref clusterBlue);
+                }
             }
-
+            
         }
     }
 }
